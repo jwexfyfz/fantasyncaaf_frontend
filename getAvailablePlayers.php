9e225203a099e02f1277deb5bf9725b83d8f5325
CC <cauchy.choi@gmail.com>
2018-06-26 23:37:33 -0700

    testing new getAvailablePlayers

--- getAvailablePlayers.php
+++ getAvailablePlayers.php
@@ -57,7 +57,7 @@
     $result = $conn->query($sql);
 
     $index = 0;
-	var $playerArray = array();
+	var $playerArray = [];
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
