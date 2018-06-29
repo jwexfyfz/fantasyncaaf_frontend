5cdd3bd8fd0c34c71feb7260e2c6cbace3f019a2
CC <cauchy.choi@gmail.com>
2018-06-28 18:42:58 -0700

    testing color text for 4 and 5 uses

--- testpage2.js
+++ testpage2.js
@@ -628,6 +628,12 @@ function populateChoosePlayerLists(inputPosition, positionList, currentSelectedP
 		}
 	}
 	select.value = currentSelectedPlayer;
+	if (positionList[i]["timesUsed"] >= 5) {
+		select.value.style.color="red";
+	}
+	else if (positionList[i]["timesUsed"] == 4) {
+		select.value.style.color="#FFA500";
+	}
 };
 
 //jeffwang 3/24/2018: This function will unhide the hidden checkmarks to tell user that the player change was successfully made.  It will then quickly re-hide it.
