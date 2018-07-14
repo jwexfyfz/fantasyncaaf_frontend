d2ed32796bf766a77a2c34d09f8222896da7e78d
CC <cauchy.choi@gmail.com>
2018-07-14 14:03:48 -0700

    testing login system

--- source/include/fg_membersite.php
+++ source/include/fg_membersite.php
@@ -617,7 +617,7 @@ class FGMembersite
         $formvars['email'] = $this->Sanitize($_POST['email']);
         $formvars['username'] = $this->Sanitize($_POST['username']);
         $formvars['password'] = $this->Sanitize($_POST['password']);
-		$this->HandleDBError($formvars);
+		$this->HandleDBError($_POST['username']);
 		return $formvars;
     }
     
