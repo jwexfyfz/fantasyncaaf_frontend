c2c07676338ec036c852e67d99fad54fe38d5686
CC <cauchy.choi@gmail.com>
2018-09-08 11:17:40 -0700

    fixing bug where populatePoints doesn't account for empty playerName

--- matchup.js
+++ matchup.js
@@ -407,7 +407,7 @@ function getFantasyPoints(week, homeOrAway, roster) {
 
 function populatePoints(homeOrAway, playerPoints, playerName, position) {
   console.log("playerPoints: "+playerName+" "+playerPoints[playerName]);
-  if(	playerPoints[playerName] != undefined	&& playerName != "") {
+  if(	playerPoints[playerName] != undefined	&& playerName != "" && playerName != undefined && playerName != null) {
 	  $('#'+homeOrAway+position+"Points").html(playerPoints[playerName]);
   }
   else {
