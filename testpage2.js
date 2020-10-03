aa454cd2a0e0b23802f817950766371d6addae1c
Jeff <jwexfyfz@gmail.com>
2020-10-03 12:05:55 -0700

    debugging disable player logic

--- testpage2.js
+++ testpage2.js
@@ -1218,7 +1218,9 @@ function populateChoosePlayerLists(inputPosition, positionList, currentSelectedP
 		}
 	}
 	else {
+		console.log("positionList.length = "+positionList.length);
 		for (i = 0; i < positionList.length; i++) {  // i = 1 to ignore --Clear Selection--
+			console.log("i="+i);
 			var gametime = new Date(positionList[i]["gametime"].replace(" ","T") + "+00:00");
 			if (	(positionList[i]["timesUsed"] >= 5)	||	(Date.now() > gametime.getTime())	) {
 				select.options[i+1].disabled = true;	// i+1 to ignore --Clear Selection--
