2261305a6478dd0516f0c76b63785efb399b07d9
Cauchy Choi <cauchy.choi@gmail.com>
2020-11-07 01:31:06 -0800

    fixing dupeTeams count bug

--- testpage2.js
+++ testpage2.js
@@ -796,7 +796,7 @@ function teamDupes(week, fantasyID, numDupeTeamsAllowed, position, teamRoster, t
 			console.log("selectedPlayerTeam: "+selectedPlayerTeam);
 			console.log("newPosition: "+newPosition);
 			
-			if ((selectedPlayerTeam != positionToTeam[newPosition] || selectedPlayerTeam != undefined) && (counts[selectedPlayerTeam] >= 2 || dupeTeams >= numDupeTeamsAllowed)/* || moreThanTwoDupeTeams*/) {  // If selected team has >= 1 use and we've hit the limit of dupe teams
+			if (selectedPlayerTeam != positionToTeam[newPosition] && selectedPlayerTeam != undefined && (counts[selectedPlayerTeam] >= 2 || dupeTeams >= numDupeTeamsAllowed)/* || moreThanTwoDupeTeams*/) {  // If selected team has >= 1 use and we've hit the limit of dupe teams
 				console.log("CHANGE NOT ALLOWED FOR " + selectedPlayerTeam);
 				
 				//No need to revert back to the original player's gametime unless the change is allowed, since we never made the change to the new player's gametime
