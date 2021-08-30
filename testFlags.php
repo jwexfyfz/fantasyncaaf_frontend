ec89ca73a8fcb95e34a4631e5ebc7ddc8e1b8bfc
jeffwang <jeffwang@JeffMac.local>
2021-08-30 10:28:24 -0700

    fixed sql query

--- testFlags.php
+++ testFlags.php
@@ -14,7 +14,7 @@
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            echo $row["isRotoLeague"];
+            echo $row["val"];
         }
     } else {
         echo "0 results";
