c7f4aeb287fa1081840853b5cf0b8f7877e02cd8
jeffwang <jeffwang@JeffMac.local>
2021-09-24 23:05:00 -0700

    jeff testing random stuff

--- jeff_playground/scripts.js
+++ jeff_playground/scripts.js
@@ -1,22 +1,78 @@
-// Replace ./data.json with your JSON feed
-fetch('https://openapi.etsy.com/v3/application/shops/6931850/listings/active', {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': 'sbz7vzg447solgzni8bfom7s'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  })
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    // Work with JSON data here
-    console.log(data)
-  })
-  .catch((err) => {
-    // Do something for an error here
-  })
+import { write } from 'fast-csv';
+import { createWriteStream } from 'fs';
+const ws = createWriteStream("listings.csv");
+
+var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+var metadata;
+var listings =[];
+
+$( document ).ready(function() {
+    getNumListings({
+        method: 'GET',
+        url: 'https://openapi.etsy.com/v3/application/shops/6931850/listings/active'
+    });
+
+    //Wait for all the GET requests to finish before using final data
+    setTimeout(() => {  
+        console.log(metadata);
+        console.log(listings);
+
+        writeToCSV(metadata, listings);
+    }, 3000);
+});
+
+function getNumListings(options) {
+    var x = new XMLHttpRequest();
+    x.open(options.method, cors_api_url + options.url);
+    x.onload = x.onerror = function () {
+        var phpResponse = JSON.parse(x.responseText);
+        var numListings = phpResponse["count"];
+        console.log(numListings);
+
+        metadata = Object.keys(phpResponse["results"][0]);
+
+        var numListingsLeft = numListings;
+        var offset = 0;
+        var urlString;
+        do {
+            urlString = 'https://openapi.etsy.com/v3/application/shops/6931850/listings/active?limit=100&offset=' + offset.toString();
+            
+            console.log(numListingsLeft);
+            console.log(offset);
+            console.log(urlString);
+
+            doCORSRequest({
+                method: 'GET',
+                url: urlString,
+            });
+            numListingsLeft = numListingsLeft - 100;
+            offset += 100;
+        } while (numListingsLeft > 0)
+    };
+
+    x.setRequestHeader('x-api-key', 'sbz7vzg447solgzni8bfom7s');
+    x.send(options.data);
+}
+
+function doCORSRequest(options) {
+    var x = new XMLHttpRequest();
+    x.open(options.method, cors_api_url + options.url);
+    x.onload = x.onerror = function () {
+        var phpResponse = JSON.parse(x.responseText);
+        var output = phpResponse["results"];
+
+        metadata = Object.keys(output[0]);
+
+        for (var i = 0; i < output.length; i++) { 
+            listings.push(output[i]);   //Append next 100 (or less) listings to array that tracks all listings
+        }
+    };
+
+    x.setRequestHeader('x-api-key', 'sbz7vzg447solgzni8bfom7s');
+    x.send(options.data);
+}
+
+function writeToCSV(metadata, listings) {
+    write(listings, { headers: true })
+        .pipe(ws);
+}
